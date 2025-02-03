c845bddbb8734053cecc628173a6e7774cd4487b
Mohammad Erfan Khebrati <khebrati@gmail.com>
2025-02-03 20:11:56 +0330

    process captions string to a dict. convert all captions to lower case

--- project/captions.py
+++ project/captions.py
@@ -6,10 +6,21 @@ def load_doc(filename):
 
 
 def all_image_captions(loaded_doc):
+    captions_dict = {}
+    lines = loaded_doc.strip().split('\n')
+    for line in lines:
+        image_id, caption = line.split('\t')
+        image_name = image_id.split('#')[0]
+        if image_name not in captions_dict:
+            captions_dict[image_name] = []
+        captions_dict[image_name].append(caption.strip())
+    return captions_dict
+
+def clean_text(captions_dict):
+    for image_name in captions_dict:
+        captions_dict[image_name] = [caption.lower() for caption in captions_dict[image_name]]
+    return captions_dict
+
+def convert_to_lowercase():
 
     pass
-
-def cleaning_text(captions):
-
-    pass
-
--- project/main.py
+++ project/main.py
@@ -1,5 +1,15 @@
-from captions import load_doc
-
+from captions import load_doc,all_image_captions,clean_text
+# testing functions manually
 string = load_doc('text/Flickr8k.token.txt')
-print(string)
-print(len(string))
+# print(string)
+# print(len(string))
+
+captions_dict = all_image_captions(string)
+# for image_name, captions in captions_dict.items():
+#     print(f"{image_name}: {captions}")
+
+cleaned_captions_dict = clean_text(captions_dict)
+# for image_name, captions in captions_dict.items():
+#     print(f"{image_name}: {captions}")
+
+    
